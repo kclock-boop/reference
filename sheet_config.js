9c1d4c79e48a8560c7d21879eb9544e943922ca3
kclock-boop <kclock@naver.com>
2026-08-03 22:06:06 +0900

    Connect survey to Google Sheets web app

--- sheet_config.js
+++ sheet_config.js
@@ -3,7 +3,7 @@ window.TAEHWA_CONFIG = {
   // Google Sheets 중앙 저장으로 바꾸려면 아래 두 값을 함께 수정해 주세요.
   // 1) storageMode: "google-sheets"
   // 2) googleSheetsWebAppUrl: 배포한 Apps Script 웹앱 URL
-  storageMode: "local",
-  googleSheetsWebAppUrl: "",
+  storageMode: "google-sheets",
+  googleSheetsWebAppUrl: "https://script.google.com/macros/s/AKfycbxmKRBpoTfcTmCEPtQEKRcVJOemWPZ-g1XnS3l66TWRXahCwXWwVPDvJJ67Hmox-Dg4oQ/exec",
   saveFallbackToLocal: true
 };
